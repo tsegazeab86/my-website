0283a67510e2a1897bac53eb61668f881bfa95cf
tsegaweb <tsegazeab860@gmail.com>
2026-02-28 12:53:59 -0800

    waw

--- Phase 3/netflix-clone/src/App.js
+++ Phase 3/netflix-clone/src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
-
+// import SearchIcon from '@mui/icons-material/Search';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
+// import AccountBoxIcon from '@mui/icons-material/AccountBox';
+// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+// import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+// import TelegramIcon from '@mui/icons-material/Telegram';
+// import YouTubeIcon from '@mui/icons-material/YouTube';
+// import InstagramIcon from '@mui/icons-material/Instagram';
 function App() {
   return (
     <div className="App"> 
